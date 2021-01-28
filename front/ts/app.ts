81b088c3eb278363dddc1a61a1c7012de40a6bf4
David Neilsen <d.neilsen@we-are-mea.com>
2021-01-29 01:14:45 +1300

    Override promise

--- front/ts/app.ts
+++ front/ts/app.ts
@@ -8,6 +8,10 @@ const app = angular.module('App', [
     'ngRoute'
 ]);
 
+app.run(['$window', '$q', function ($window, $q) {
+    $window.Promise = $q;
+}]);
+
 (window as any).app = app;
 (window as any).JsonApi = JsonApi;
 
