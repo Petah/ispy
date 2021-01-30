cd1d4926003e6fd127473157954a54d30c2dc03f
David Neilsen <d.neilsen@we-are-mea.com>
2021-01-31 01:11:45 +1300

    Server

--- bin/server.sh
+++ bin/server.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-pushd $DIR/../server/
-PHP_CLI_SERVER_WORKERS=$(getconf _NPROCESSORS_ONLN)
-php artisan serve --port=9511 --host=0.0.0.0
+pushd $DIR/../
+git pull
+npm i
+pushd front
+../node_modules/.bin/gulp
+popd
+./node_modules/.bin/forever start --minUptime 1000 --spinSleepTime 1000 -c node_modules/ts-node/dist/bin.js socket/src/server.ts
 popd
